de1dd9922c52095fad9437cd3780d657eca3e24b
elno <elno.brianardi@gmail.com>
2025-08-12 16:49:41 +0700

    fix layout in market page

--- landingpage/src/components/MarketHeroSection.jsx
+++ landingpage/src/components/MarketHeroSection.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import HeroImage from '../assets/Hero_Section/Image_Inside_Banner_(Hero_Section).png'
 const MarketHeroSection = () => {
   return (
-    <div className='flex p-16 items-center justify-center'>
-          <div className='text-white p-16 gap-8'>
+    <div className='flex py-16 items-center justify-center'>
+          <div className='text-white py-16 gap-8'>
             <h2 className='text-[52px] font-bold'>Welcome to Bitzaro</h2>
             <p>Engineering the Digital Economy</p>
             <button className='rounded py-2 px-5 bg-[#7815F9] my-8'>
--- landingpage/src/pages/Market.jsx
+++ landingpage/src/pages/Market.jsx
@@ -12,11 +12,11 @@ const Market = () => {
     <div className="min-h-screen w-full bg-black">
       <NavigationSection />
 
-      <section className="pt-[80px]">
+      <section className="pt-[80px] max-w-[1200px] mx-auto">
         <MarketHeroSection />
       </section>
 
-      <section className="grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-[1200px] mx-auto">
+      <section className="grid gap-4 py-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full max-w-[1200px] mx-auto">
       {lineChartMockData.map((item) => (
         <LineChartCard key={item.id} {...item} />
       ))}
